406fecdbefe9fe53b4bb9bad0024423d51068c3a
42 Development Studio <42devstudio@gmail.com>
2016-02-02 18:24:19 +0100

    comments

--- fuel/app/views/templates/new_zhitnitca/content/comments_block.php
+++ fuel/app/views/templates/new_zhitnitca/content/comments_block.php
@@ -1,6 +1,14 @@
 <div class="commetnts-wrp" data-ng-app="comments" data-ng-controller="comment" data-content-id="<?= $content->id;?>">
     <h5 data-ng-click="toggleHeight()">Комментарии ({{count}}) <i class="glyphicon glyphicon-chevron-down"></i></h5>
     <div data-ng-class="{close: collapsed}">
+        
+        <div class="comments" >
+            <div class="comment_item" data-ng-repeat="comment in comments_list ">
+                <h5>{{comment.user_name}} {{(comment.created_at * 1000) | date:'HH:mm yyyy-MM-dd'}}</h5>
+                <p>{{comment.text}}</p>
+            </div>
+        </div>
+        
         <div class="fotm-wrp comment-form" >
             <div class="form-group" data-ng-class="{'has-error' : errors['comment-name']}" >
                 <label for="comment-name">Имя</label>
@@ -24,12 +32,6 @@
                 <div class="col-md-6 ">
                     <button data-ng-class="{disabled: capcha == ''}" class="btn btn-success btn-lg pull-right " data-ng-click="addComment(user_name, user_email, comment_text)">Отправить</button>
                 </div>
-            </div>
-        </div>
-        <div class="comments" >
-            <div class="comment_item" data-ng-repeat="comment in comments_list ">
-                <h5>{{comment.user_name}} {{(comment.created_at * 1000) | date:'HH:mm yyyy-MM-dd'}}</h5>
-                <p>{{comment.text}}</p>
             </div>
         </div>
     </div>
