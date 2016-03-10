4d45154e09839fa97d7989ced174a32a8154a68f
42 Development Studio <42devstudio@gmail.com>
2016-03-10 17:17:53 +0100

    marengo

--- fuel/app/views/templates/marengo/home.php
+++ fuel/app/views/templates/marengo/home.php
@@ -6,7 +6,7 @@
         <title><?= isset($title) ? $title : '' ?></title>
         <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
         <? if(isset($meta_keywrd)): ?>
             <meta name="keywords" content="<?= $meta_keywrd;?>">
         <? endif; ?>
