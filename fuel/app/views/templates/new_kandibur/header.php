84005f9a5af559d310b124fff141e9c7f5c49392
42 Development Studio <42devstudio@gmail.com>
2016-03-14 17:34:53 +0100

    kb

--- fuel/app/views/templates/new_kandibur/header.php
+++ fuel/app/views/templates/new_kandibur/header.php
@@ -23,7 +23,7 @@
                 <?php endforeach; ?>
                 
             </ul>
-            <? if(Model_Language::query()->count() > 0):?>
+            <? if(Model_Language::query()->count() > 1):?>
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown ">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= TCLocal::getCurrentLangModel()->name; ?> <i class="glyphicon glyphicon-cog"></i></a>
