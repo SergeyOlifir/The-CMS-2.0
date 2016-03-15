4646aff6a13a661d84b882913fe34b686d77776b
42 Development Studio <42devstudio@gmail.com>
2016-03-15 14:26:12 +0100

    kb

--- fuel/app/views/templates/new_kandibur/home/partials/home.php
+++ fuel/app/views/templates/new_kandibur/home/partials/home.php
@@ -9,20 +9,6 @@
                     <div class="row">
                         <? $i = 0; ?>
                         <? foreach ($rcategory->get_content(4, 0) as $cont): ?>
-                            <!--<div class="col-sm-6 col-xs-12">
-                                <a class="feat-wrp" href="<?= TCRouter::get('view_subsidiary_content', array('lang' => TCLocal::getCurrentLang(), 'id' => $cont->id, 'parent_category' => $rcategory->id)) ;?>">
-                                    <div class="clearfix">
-                                        <div class="text-area <?= (($i < 2) ? 'pull-left' : 'pull-right'); ?>" data-position="<?= $i; ?>">
-                                            <? $category = ((count($cont->master_category) > 0) ? array_pop($cont->master_category) : $rcategory); ?>
-                                            <h4 class="cat-title <?= (($i < 2) ? 'top' : 'bottom'); ?>"><?= $category->title; ?></h4>
-                                            <h4 class="cont-title <?= (($i < 2) ? 'bottom' : 'top'); ?>"><?= $cont->title; ?></h4>
-                                        </div>
-                                        <div class="image-area <?= (($i < 2) ? 'pull-right' : 'pull-left'); ?>">
-                                            <div class="bgr" style="background-image: url('<?= $cont->get_logo('small');?>')"></div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>-->
                             <div class="col-xs-12">
                                 <div class="clearfix">
                                     <? if(!($i % 2)): ?>
@@ -70,6 +56,23 @@
     </div>
 </div>
 
+<div class="container">
+    <div class="related-categoryes-wrp main">
+        <? if(count($main_page_model->promoted_category) > 0): ?>
+            <? foreach($main_page_model->promoted_category as $rcategory): ?>
+                <? if(count($rcategory->get_own_content(4)) > 0): ?>
+                    <div class="related-category">
+                        <h2 class="related-cat-title"><?= Fuel\Core\Html::anchor(TCRouter::get('view_category', array('alias' => $rcategory->alias)), $rcategory->title);?></h2>
+                        <div class="row tiles">
+                            <?= \TCCore\TCTheme::render("content/tiles", array('content' => $rcategory->get_own_content(4), 'category' => $rcategory, 'fool_view' => false)); ?>
+                        </div>
+                    </div>
+                <? endif; ?>
+            <? endforeach; ?>
+        <? endif; ?>
+    </div>
+</div>
+
 <div class="featured-wrp-type2">
     <div class="container">
         <? if(count($main_page_model->get_featured_categories(2)) > 0): ?>
@@ -92,25 +95,3 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="related-categoryes-wrp">
-        <? if(count($main_page_model->promoted_category) > 0): ?>
-            <? foreach($main_page_model->promoted_category as $rcategory): ?>
-                <? if(count($rcategory->get_own_content(4)) > 0): ?>
-                    <div class="related-category">
-                        <h2 class="related-cat-title"><?=$rcategory->title; ?></h2>
-                        <div class="row tiles">
-                            <?= \TCCore\TCTheme::render("content/tiles", array('content' => $rcategory->get_own_content(4), 'category' => $rcategory, 'fool_view' => false)); ?>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-9"></div>
-                            <div class="col-md-3">
-                                <h4 class="page-count"><?= Fuel\Core\Html::anchor(TCRouter::get('view_category', array('alias' => $rcategory->alias)), 'Все статьи раздела', array('class' => 'page-count'));?></h4>
-                            </div>
-                        </div>
-                    </div>
-                <? endif; ?>
-            <? endforeach; ?>
-        <? endif; ?>
-    </div>
-</div>
