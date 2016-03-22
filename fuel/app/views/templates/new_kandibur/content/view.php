ca2c9e4a59526fc0722a59a74da7b1364880ca52
42 Development Studio <42devstudio@gmail.com>
2016-03-22 22:14:37 +0100

    kb

--- fuel/app/views/templates/new_kandibur/content/view.php
+++ fuel/app/views/templates/new_kandibur/content/view.php
@@ -6,46 +6,43 @@
         <h2><?= $content->title; ?></h2>
         <div class="col-md-9">
             
-            <ol class="breadcrumb">
+            <!--<ol class="breadcrumb">
                 <li><a href="/"><i class="glyphicon glyphicon-home"></i></a></li>
                 <? if(!is_null($parent_category)): ?>
                     <li><?= Fuel\Core\Html::anchor(TCRouter::get('view_category', array('alias' => $parent_category->alias)), $parent_category->title) ;?></li>
                 <? endif;?>
                 <li><a><?= $content->title; ?></a></li>
-            </ol>
-            <div class="row">
-                <div class="col-sm-8 col-xs-12">
-                    <div id="content-carousel" class="carousel slide content-galery-carusel">
-                        <div class="carousel-inner">
-                            <? $i = 0; ?> 
-                            <? foreach($content->get_images() as $image): ?>
-                                <div class="<?= $i == 0 ? 'active' : '' ?> item" style="background-image: url('/files/<?= $image->galery; ?>')">
-                                    <!--<a class="fancybox" data-fancybox-group="button" href="<?= Fuel\Core\Uri::base() . "files/{$image->full}" ?>"><?= Html::img("files/{$image->galery}"); ?></a>-->
-                                </div>
-                                <? $i++; ?>
-                            <? endforeach; ?>
+            </ol>-->
+            <div class="content-bg">
+                <div class="row">
+                    <div class="col-sm-8 col-xs-12">
+                        <div id="content-carousel" class="carousel slide content-galery-carusel">
+                            <div class="carousel-inner">
+                                <? $i = 0; ?> 
+                                <? foreach($content->get_images() as $image): ?>
+                                    <div class="<?= $i == 0 ? 'active' : '' ?> item" style="background-image: url('/files/<?= $image->galery; ?>')">
+                                        <!--<a class="fancybox" data-fancybox-group="button" href="<?= Fuel\Core\Uri::base() . "files/{$image->full}" ?>"><?= Html::img("files/{$image->galery}"); ?></a>-->
+                                    </div>
+                                    <? $i++; ?>
+                                <? endforeach; ?>
+                            </div>
+                            <ol class="carousel-indicators">
+                                <? $i = 0; ?> 
+                                <? foreach($content->get_images() as $image): ?>
+                                    <li data-target="#content-carousel" style="background-image: url('/files/<?= $image->galery; ?>')" class="<?= $i == 0 ? 'active' : '' ?>" data-slide-to="<?= $i++; ?>"></li>
+                                <? endforeach; ?>
+                            </ol>
                         </div>
-                        <ol class="carousel-indicators">
-                            <? $i = 0; ?> 
-                            <? foreach($content->get_images() as $image): ?>
-                                <li data-target="#content-carousel" style="background-image: url('/files/<?= $image->galery; ?>')" class="<?= $i == 0 ? 'active' : '' ?>" data-slide-to="<?= $i++; ?>"></li>
-                            <? endforeach; ?>
-                        </ol>
+                    </div>
+                    <div class="col-sm-4 col-xs-12 details">
+                        <div>
+                            <?= $content->description; ?>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-4 col-xs-12 details">
-                    <? if(!is_null($parent_category)): ?>
-                        <p class="sm">Раздел: <?= Fuel\Core\Html::anchor(TCRouter::get('view_category', array('alias' => $parent_category->alias)), $parent_category->title) ;?></p>
-                    <?endif; ?>
-                    <p class="sm">Количество комментариев: <a href="#">27</a></p>
-                    <p class="sm">Автор: <a href="#">ТМ «Рiдна Житница» </a></p>
-                    <p class="sm">Дата: <?= Date::forge($content->created_at)->format("%d.%m.%Y", true); ?></p>
-                </div>
-            </div>
-            <div class="description">
-                <p>
+                <div class="description">
                     <?= $content->content; ?>
-                </p>
+                </div>
             </div>
             
             <?= TCCore\TCTheme::render('content/comments_block'); ?>
