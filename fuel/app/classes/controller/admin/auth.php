112d42d98ef17038965f1206f180e6c9191f0663
42 Development Studio <42devstudio@gmail.com>
2016-03-30 17:04:59 +0200

    admin template

--- fuel/app/classes/controller/admin/auth.php
+++ fuel/app/classes/controller/admin/auth.php
@@ -8,7 +8,6 @@ class Controller_Admin_Auth extends Controller_Admin {
     public function action_login() {
         if(\Fuel\Core\Input::post()) {
             if (\Auth::login(\Fuel\Core\Input::post('username'), \Fuel\Core\Input::post('password'))) {
-                Fuel\Core\Session::set_flash('success', 'Угадал');
                 Fuel\Core\Response::redirect('/admin/dashboard');
             } else {
                 Fuel\Core\Session::set_flash('error', 'Не угадал');
