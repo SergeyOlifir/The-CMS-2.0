69f2d5cad14eeeca62f9faf5f7baa953de8b9d9c
42 Development Studio <42devstudio@gmail.com>
2016-08-21 14:36:44 +0200

    icons

--- fuel/app/views/templates/new_kandibur/footer.php
+++ fuel/app/views/templates/new_kandibur/footer.php
@@ -1,8 +1,22 @@
 <div class="container">
     <div class="container">
         <div class="bnr-wrp">
-            <a href="#"><?= Fuel\Core\Html::img('/assets/img/templates/new_kandibur/bnr.jpg');?></a>
+            <a href="http://kandibur.info/shkola-kandibura"><?= Fuel\Core\Html::img('/assets/img/templates/new_kandibur/bnr.jpg');?></a>
         </div>
     </div>
+    <!--<ul class="soc-buttons">
+        <li class="fb-icon">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+        </li>
+        <li class="vk-icon">
+            <a href="#"><i class="fa fa-vk"></i></a>
+        </li>
+        <li class="yo-icon">
+            <a href="#"><i class="fa fa-youtube"></i></a>
+        </li>
+        <li class="ok-icon">
+            <a href="#"><i class="fa fa-odnoklassniki"></i></a>
+        </li>
+    </ul>-->
     <?= $main_page_model->footer_content; ?>
 </div>
